4a9a7d4b860318e360091f229670e1c0ee0fd5c6
Huy-DNA <huydo862003@gmail.com>
2026-05-10 00:35:21 +0700

    chore: basic integration of design system to the blog

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a
-      href="https://vuejs.org/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >vuejs.org</a> to read the
-    documentation
-  </p>
+  <div
+    class="group absolute w-full overflow-visible h-16"
+  >
+    <GNavbar
+      class="transition-transform duration-300 ease-out -translate-y-full group-hover:translate-0"
+    >
+      <template #left>
+        <GNavBrand
+          :as="RouterLink"
+          class="cursor-pointer"
+          to="/"
+        >
+          <h1 class="text-lg">Starfall Meadow</h1>
+        </GNavBrand>
+      </template>
+      <template #right>
+        <GNavLink
+          :as="RouterLink"
+          to="/posts"
+          label="Posts"
+          :icon="GIconName.Newspaper"
+          class="text-sm my-1"
+        />
+      </template>
+    </GNavBar>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { GIconName, GNavbar, GNavBrand, GNavLink, GTheme, useTheme } from '@hdnax/genuix';
+import { RouterLink } from 'vue-router';
+
+// Always use dark theme for design system
+const theme = useTheme();
+theme.setTheme(GTheme.Dark);
+</script>
